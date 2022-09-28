fb40e9726d7ed049a27abe9e3864371c18648e68
qwerq123456 <qwerq123456@naver.com>
2022-09-28 17:04:40 +0900

    FIX get method index error

--- src/GudgleList.java
+++ src/GudgleList.java
@@ -98,7 +98,7 @@ public class GudgleList<E> {
         }
         if(index == 0) return head.data;
         Node<E> temp = head;
-        for(int i = 0; i<index-1; i++) {
+        for(int i = 0; i<index; i++) {
             temp = temp.next;
         }
         return temp.data;
